b9546a1f435359efa527ac995d804055b8fe8616
Shivani Gupta <guptashivani312002@gmail.com>
2024-07-15 21:05:06 +0530

    demo2

---
+++ script.js
@@ -0,0 +1,16 @@
+/*for(let i=0;i<5;i++)
+{
+    console.log('hey guys this is shivani gupta',i)
+}
+let arr=["shivani","khushboo","nisha","riya"]
+for (let i in arr){
+console.log(arr[i])
+}
+for (let i of arr){
+    console.log(i)
+    }*/
+let x="shivani gupta"
+let y=45
+let z=67.98
+let a=true
+console.log(typeof(x)," ",typeof(y)," ",typeof(z)," ",typeof(a))
